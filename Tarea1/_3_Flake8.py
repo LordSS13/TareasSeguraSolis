141afbb9378a52b4c1a56bbda741a041a043497e
LordSS13 <99686766+LordSS13@users.noreply.github.com>
2022-02-18 19:43:35 -0600

    Delete _3_Flake8.py

--- Tarea1/_3_Flake8.py
+++
@@ -1,7 +0,0 @@
-x=6  # Error 1 (Espacios en las condiciones de igualdad.)
-if x < 5:
-    print("x es menor que 5")
-elif x == 5:
-    print("Son las decisiones las que nos hacen ser quienes somos, y siempre podemos optar por hacer lo correcto.")  # Error 2 (Línea demasiado extensa)
-else:
-    print("x es mayor que 5")  # Error 3 (Falta última línea en blanco.)
