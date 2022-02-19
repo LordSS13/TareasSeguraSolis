babc327eba065660f050fe6a493d3d7976aa8745
LordSS13 <99686766+LordSS13@users.noreply.github.com>
2022-02-18 19:43:21 -0600

    Delete _0_Flake8.py

--- _0_Flake8.py
+++
@@ -1,7 +0,0 @@
-x = 6  # Error 1 (Espacios en las condiciones de igualdad.)
-if x < 5:
-    print("x es menor que 5.")
-elif x == 5:
-    print("x es igual que 5.")  # Error 2 (Línea demasiado extensa)
-else:
-    print("x es mayor que 5.")  # Error 3 (Falta última línea en blanco.)
